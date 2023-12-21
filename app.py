baca631f786a13b073519ce8550451a8ae8cfa11
Tran Thanh Hai <tranthanhhai0812@gmail.com>
2023-12-21 19:47:31 +0700

    fix: replace the DNN by SVM

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ import pandas as pd
 import os
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from flask_cors import CORS
+import pickle
 
 
 app = Flask(__name__)
 CORS(app)
-model = load_model('./model')
+with open('svm_model.pkl', 'rb') as model_file:
+    loaded_svm_model = pickle.load(model_file)
 df_train = pd.read_csv('./data/datatrain.csv')
 df_train = df_train.drop('Occupancy', axis=1).drop('date', axis=1)
 scaler = StandardScaler()
@@ -50,7 +52,7 @@ def postprocess(prediction):
 def predict():
     data = request.get_json(force=True)
     input_data = preprocess(data)
-    prediction = model.predict(input_data)
+    prediction = loaded_svm_model.predict(input_data)
     result = postprocess(prediction)
     return jsonify(result)
 
